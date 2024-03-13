07f7bbebbd1f48bf49623824e49bf9c54cdc044d
sophia luna <137638625+sophia-luna@users.noreply.github.com>
2024-03-13 11:39:08 -0300

    Add files via upload

---
+++ GC.WebAPI/Program.cs
@@ -0,0 +1,35 @@
+using System.Text.Json.Serialization;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+var app = builder.Build();
+
+
+//Enable CORS
+app.UseCors(c=>c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
